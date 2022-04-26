0b7db0bb6cb3d36742142470656710e1dfbfa09b
EvelynG02 <90287881+EvelynG02@users.noreply.github.com>
2022-04-25 20:39:17 -0400

    Update script.js

--- studio/project003+/script.js
+++ studio/project003+/script.js
@@ -39,5 +39,89 @@ fetch('https://api.airtable.com/v0/app6gezkzOPuEsZfV/mushroom', {
 
       // that's it!
       // Try adding or removing items in your Airtable base and see your website update on refresh
+
+
+
+
+      // first we set up our global variables to track initial state of our filter parameters
+let sliderValue = 50;
+let mushroomType = 'all';
+
+// update sorting
+function handleSort() {
+  sortBy = event.target.value;
+  generateContent(); // after we update sortBy variable, we can generateContent() again to capture the new filters
+}
+
+
+// update dropdown
+function handleDropdown() {
+  mushroomType = event.target.value;
+  generateContent();
+}
+
+// update slider
+const sliderDisplayValue = document.querySelector('.range-slider-container span'); // slider HTML display value
+function handleSlider() {
+  sliderValue = event.target.value;
+  sliderDisplayValue.innerHTML = `$${numberWithCommas(sliderValue)}`; // update slider value. Sending new value through numberWithCommas function below to get commas  in display for 1,000+
+  generateContent();
+}
+
+// these html elements represent our interactive elements
+const reset = document.querySelector('.reset');
+const dropdown = document.querySelector('.dropdown-container select');
+const checkbox = document.querySelector('.checkbox-container input');
+const slider = document.querySelector('.range-slider-container input');
+const radio = document.querySelector('.radio-container #popularity');
+
+// clicking "reset" button will set all variables back to initial state and then update our interactive html elements to display the new state
+function resetFilters() {
+  mushroomType = 'all';
+  sliderValue = 50;
+  sliderDisplayValue.innerHTML = `$${numberWithCommas(sliderValue)}`;
+  slider.value = sliderValue;
+  dropdown.value = 'all';
+  generateContent();
+}
+
+// content
+const content = document.querySelector('.content'); // empty placeholder div in our HTML for our data-driven content
+function generateContent() {
+  content.innerHTML = ''; // important! we want to delete all content on the screen each time we re-run to capture new filter and sorting parameters
+  data
+    .filter(item => {
+      return item.height <= sliderValue; // this filter only returns items that are priced less than slider value
+    })
+    
+    .filter(item => {
+      return mushroomType === 'all' ? item : item.type === mushroomType; // if our dropdown is set to all, return evey item, otherwise only return items that match selected instrument type
+    })
+    .sort((a, b) => a[sortBy] - b[sortBy]) // sort by value from radio buttons
+    .forEach(item => {
+      // eveything in this forEach is the same as your project 3 Airtable examples
+      content.innerHTML += `
+      <div class="mushroom">
+  
+          
+          <h3>${mushroom.fields.name}</h3>
+          <img src="${mushroom.fields.image[0].thumbnails.large.url}" width='250'/>
+          
+         <h4>${mushroom.fields.health_benefits}</h4>
+         <h7>${mushroom.fields.height}</h7>
+         <h6>${mushroom.fields.season}</h6>
+          <h5>${mushroom.fields.type}</h5> 
+
+
+        </div>
+    `;
+    });
+}
+generateContent();
+
+// utility function to display numnbers with commas if they are 1,000+
+function numberWithCommas(num) {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
     });
   });
